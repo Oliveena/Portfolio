d9dfb357d8ea85e462a7d58652ffde9f13fce673
Ana Tara <ana.tara@outlook.com>
2025-07-03 10:01:21 -0400

    updated CV section and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,8 @@ export default function Footer() {
                 <a href="https://www.linkedin.com/in/anastassia-tarassova-4a6573188/?trk=opento_sprofile_topcard" target="_blank" rel="noopener noreferrer">
                     <img src="/images/linkedin-logo-linkedin-icon-transparent-free-png.webp" alt="LinkedIn logo" width="50" height="50" />
                 </a>
-                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-                    <img src="/images/1725819461instagram-logo.png" alt="Instagram logo" width="50" height="50" />
-                </a>
-                <a href="https://www.meta.com/ca/" target="_blank" rel="noopener noreferrer">
-                    <img src="/images/Meta-Logo.png" alt="META logo" width="50" height="50" />
-                </a>
-                <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-                    <img src="/images/98ae159aa1bb8b4243fbe1f9e27d1b9f.png" alt="YouTube logo" width="50" height="50" />
-                </a>
             </div>
+            <p>Built with React and Bootstrap</p>
             <p>&copy; {new Date().getFullYear()} Anastassia Tarassova. All rights reserved.</p>
         </footer>
     );
